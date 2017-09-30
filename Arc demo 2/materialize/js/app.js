49c0d6e5ecaa168c9c5743386c70f165a2a4ddc1
Mint Winchester <alphamega1923@gmail.com>
2017-09-30 16:41:48 +0400

    Add files via upload

---
+++ Arc demo 2/materialize/js/app.js
@@ -0,0 +1,36 @@
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+particlesJS.load('particles-js-ai', 'particles-ai.json', function() {
+  console.log('particles.js loaded - callback');
+});
+
+$.fn.moveIt = function() {
+  var $window = $(window);
+  var instances = [];
+
+  $(this).each(function() {
+    instances.push(new moveItItem($(this)));
+  });
+
+  window.onscroll = function() {
+    var scrollTop = $window.scrollTop();
+    instances.forEach(function(inst) {
+      inst.update(scrollTop);
+    });
+  };
+};
+
+var moveItItem = function(el) {
+  this.el = $(el);
+  this.speed = parseInt(this.el.attr("data-scroll-speed"));
+};
+
+moveItItem.prototype.update = function(scrollTop) {
+  this.el.css("transform", "translateY(" + -(scrollTop / this.speed) + "px)");
+};
+
+// Initialization
+$(function() {
+  $("[data-scroll-speed]").moveIt();
+});
